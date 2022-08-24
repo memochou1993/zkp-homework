04c9211877ad5b35e4189e0b8679c4e57247516f
Memo Chou <memochou1993@gmail.com>
2022-08-25 00:15:22 +0800

    Remove perm function

--- main.js
+++ main.js
@@ -20,71 +20,27 @@ document.getElementById('resolveProblem3').onclick = async () => {
   console.log(res);
 }
 
-const root = '0xe2de7e936cd2e3b398a5b5b89726a8d72148b93050d271cf91ffec3cc5598577';
-
-const elements = [
+const nodes = [
+  'zkplayground',
   'zkpenguin',
   'zkpancake',
   'zkpolice',
   'zkpig',
-  'zkplayground',
   'zkpigeon',
-  'zkpoison'
+  'zkpoison',
 ];
 
-const groups = perm(elements);
+const leaves = nodes.map((ids) => ethers.utils.id(ids));
 
-let target;
+const tree = new window.MerkleTree(leaves, ethers.utils.keccak256, {
+  sortPairs: true,
+});
 
-for (const nodes of groups) {
-  const leaves = nodes.map((node) => ethers.utils.id(node));
-  const tree = new window.MerkleTree(leaves, ethers.utils.keccak256, { sortPairs: true });
-  if (tree.getHexRoot() === root) {
-    target = tree;
-    break;
-  }
-}
+console.log(tree.toString());
 
-const leave = ethers.utils.id('zkplayground');
-
-const proof = target.getHexProof(leave);
-
-console.log(target.toString());
-
-console.log(proof);
+const proof = tree.getHexProof(ethers.utils.id('zkplayground'));
 
 document.getElementById('resolveProblem4').onclick = async () => {
   const res = await contract.merkleProof(proof);
   console.log(res);
 };
-
-function perm(list) {
-  var pls = [];
-  if (list.length == 0) {
-    pls.push([]);
-  } else {
-    allRotated(list).forEach(function (lt) {
-      perm(lt.slice(1, lt.length)).forEach(function (tailPl) {
-        var pl = [];
-        pl.push(lt[0]);
-        pls.push(pl.concat(tailPl));
-      });
-    });
-  }
-  return pls;
-}
-
-function allRotated(list) {
-  function rotatedTo(i) {
-    var rotated = [];
-    rotated.push(list[i]);
-    return rotated
-      .concat(list.slice(0, i))
-      .concat(list.slice(i + 1, list.length));
-  }
-  var all = [];
-  for (var i = 0; i < list.length; i++) {
-    all.push(rotatedTo(i));
-  }
-  return all;
-}
